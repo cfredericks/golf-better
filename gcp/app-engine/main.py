a87f3a17aa7a6b16e8a57851edf44f37fe878993
cfredericks <cfred1985@gmail.com>
2024-07-23 20:21:26 -0700

    add auth decorator

--- gcp/app-engine/main.py
+++ gcp/app-engine/main.py
@@ -1,3 +1,4 @@
+from functools import wraps
 from flask import Flask, request
 import os
 import json
@@ -20,13 +21,25 @@ DEFAULT_VERSION_ID = 'latest'
 PUBLIC_API_PREFIX = '/api/v1'
 PRIVATE_API_PREFIX = '/protected/api/v1'
 
-def verify_id_token(id_token):
-    try:
-        decoded_token = auth.verify_id_token(id_token)
-        return decoded_token
-    except Exception as e:
-        print("Exception decoding auth token", e)
-        return None
+# Decorator to parse auth token and extract user email
+def validate_token(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        decoded_token = None
+        if 'Authorization' in request.headers:
+            id_token = request.headers.get('Authorization').split('Bearer ')[1]
+            try:
+                decoded_token = auth.verify_id_token(id_token)
+            except Exception as e:
+                print("Exception decoding auth token", e)
+                return jsonify({"error": "Unauthorized"}), 401
+
+        if not decoded_token:
+            return jsonify({"error": "Unauthorized"}), 401
+
+        user_email = decoded_token.get('email')
+        return f(user_email, *args, **kwargs)
+    return decorated_function
 
 def get_gsm_secret(secret_id, project_id=DEFAULT_PROJECT_ID, version_id=DEFAULT_VERSION_ID):
     client = secretmanager.SecretManagerServiceClient()
@@ -84,17 +97,9 @@ def get_db_connection():
     return pool
 
 @app.route(PUBLIC_API_PREFIX + '/pga-tournaments', methods=['GET'])
-def get_pga_tournaments():
-    decoded_token = None
-    if 'Authorization' in request.headers:
-        id_token = request.headers.get('Authorization').split('Bearer ')[1]
-        decoded_token = verify_id_token(id_token)
-        print(decoded_token)
-    if not decoded_token:
-        return json.dumps({"error": "Unauthorized"}), 401
-    user_email = decoded_token.get('email')
+@validate_token
+def get_pga_tournaments(user_email):
     print(f"Got request from user: {user_email}")
-
     query = "SELECT data FROM pga_tournaments where 1=1"
     tournament_id = request.args.get('id', None)
     if tournament_id is not None:
@@ -106,16 +111,9 @@ def get_pga_tournaments():
 
 
 @app.route(PUBLIC_API_PREFIX + '/pga-leaderboard-players', methods=['GET'])
-def get_pga_leaderboard_players():
-    decoded_token = None
-    if 'Authorization' in request.headers:
-        id_token = request.headers.get('Authorization').split('Bearer ')[1]
-        decoded_token = verify_id_token(id_token)
-    if not decoded_token:
-        return json.dumps({"error": "Unauthorized"}), 401
-    user_email = decoded_token.get('email')
+@validate_token
+def get_pga_leaderboard_players(user_email):
     print(f"Got request from user: {user_email}")
-
     query = "SELECT data FROM pga_leaderboard_players where 1=1"
     tournament_id = request.args.get('tournamentId', None)
     if tournament_id is not None:
@@ -129,16 +127,9 @@ def get_pga_leaderboard_players():
         return json.dumps([row[0] for row in records], default=json_serial), 200
 
 @app.route(PUBLIC_API_PREFIX + '/pga-player-scorecards', methods=['GET'])
-def get_pga_player_scorecards():
-    decoded_token = None
-    if 'Authorization' in request.headers:
-        id_token = request.headers.get('Authorization').split('Bearer ')[1]
-        decoded_token = verify_id_token(id_token)
-    if not decoded_token:
-        return json.dumps({"error": "Unauthorized"}), 401
-    user_email = decoded_token.get('email')
+@validate_token
+def get_pga_player_scorecards(user_email):
     print(f"Got request from user: {user_email}")
-
     query = "SELECT data FROM pga_player_scorecards where 1=1"
     tournament_id = request.args.get('tournamentId', None)
     if tournament_id is not None:
@@ -150,54 +141,6 @@ def get_pga_player_scorecards():
     with pool.connect() as db_conn:
         records = db_conn.execute(sqlalchemy.text(query)).fetchall()
         return json.dumps([row[0] for row in records], default=json_serial), 200
-
-@app.route(PRIVATE_API_PREFIX + '/refreshTournaments', methods=['POST'])
-def refresh_tournaments():
-    decoded_token = None
-    if 'Authorization' in request.headers:
-        id_token = request.headers.get('Authorization').split('Bearer ')[1]
-        decoded_token = verify_id_token(id_token)
-    if not decoded_token:
-        return json.dumps({"error": "Unauthorized"}), 401
-    user_email = decoded_token.get('email')
-    print(f"Got request from user: {user_email}")
-
-    print("Querying Tournaments API...")
-    sports_data_api_key = get_gsm_secret('sports-data-api-key')
-    tournaments_response = requests.get("https://api.sportsdata.io/golf/v2/json/Tournaments?key=" + sports_data_api_key)
-    tournaments = tournaments_response.json()
-    print("Got response: ")# + json.dumps(tournaments))
-
-    last_updated = datetime.utcnow()
-    tournaments_by_name_startDate = {}
-    for t in tournaments:
-        tournaments_by_name_startDate[(t['Name'], t['StartDate'])] = (
-                t["TournamentID"],
-                "'" + t["Name"].replace("'", "''") + "'",
-                "'" + t["StartDate"] + "'" if t["StartDate"] is not None else None,
-                "'" + t["EndDate"] + "'" if t["EndDate"] is not None else None,
-                "'" + str(last_updated) + "'",
-                "'" + json.dumps(t).replace("'", "''") + "'"
-            )
-
-    pool = get_db_connection()
-    with pool.connect() as db_conn:
-        try:
-            args_str = ','.join([f'({t[0]}, {t[1]}, {t[2]}, {t[3]}, {t[4]}, {t[5]})' for t in tournaments_by_name_startDate.values()])
-            #print(args_str)
-            db_conn.execute(sqlalchemy.text("INSERT INTO tournaments (id, name, start_date, end_date, last_updated, data) VALUES " \
-                + args_str \
-                + "ON CONFLICT (id) DO UPDATE SET name = EXCLUDED.name, start_date = EXCLUDED.start_date, end_date = EXCLUDED.end_date, last_updated = EXCLUDED.last_updated, data = EXCLUDED.data"))
-
-            # commit transaction (SQLAlchemy v2.X.X is commit as you go)
-            db_conn.commit()
-            print("Finished refreshing tournaments")
-            return '', 200
-        except Exception as e:
-            print("Error writing to DB: ", e)
-            raise e
-            return '', 500
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
