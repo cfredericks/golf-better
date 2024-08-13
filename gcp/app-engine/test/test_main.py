900dcaa5f9569fea9e32c04b81d07f809b760cef
cfredericks <cfred1985@gmail.com>
2024-08-13 01:49:52 -0700

    cleanup tests

--- gcp/app-engine/test/test_main.py
+++ gcp/app-engine/test/test_main.py
@@ -2,11 +2,8 @@ import main
 import json
 import pytest
 
-from datetime import date, datetime
 from unittest.mock import MagicMock
 
-
-SECRET_PAYLOAD = b'secret-value'
 
 # Mock data for the API response
 mock_records = [
@@ -15,45 +12,10 @@ mock_records = [
 ]
 
 @pytest.fixture
-def mock_secret_manager(mocker):
-    mock_client = mocker.patch('main.secretmanager.SecretManagerServiceClient')
-    mock_instance = mock_client.return_value
-    mock_response = mocker.Mock()
-    mock_response.payload.data.decode.return_value = SECRET_PAYLOAD
-    mock_instance.access_secret_version.return_value = mock_response
-    return mock_instance
-
-@pytest.fixture
 def client():
     main.app.config['TESTING'] = True
     with main.app.test_client() as client:
         yield client
-
-def test_get_gsm_secret_defaults(mock_secret_manager):
-    secret_id = 'test-secret'
-    secret = main.get_gsm_secret(secret_id)
-    mock_secret_manager.access_secret_version.assert_called_once_with(name=f"projects/{main.DEFAULT_PROJECT_ID}/secrets/{secret_id}/versions/{main.DEFAULT_VERSION_ID}")
-    assert secret == SECRET_PAYLOAD
-
-def test_get_gsm_secret_overrides(mock_secret_manager):
-    secret_id = 'test-secret'
-    project_id = 'test-project-id'
-    version_id = 'test-version-id'
-    secret = main.get_gsm_secret(secret_id, project_id, version_id)
-    mock_secret_manager.access_secret_version.assert_called_once_with(name=f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}")
-    assert secret == SECRET_PAYLOAD
-
-def test_json_serial_date():
-    d = date(2024, 1, 1)
-    assert main.json_serial(d) == "2024-01-01"
-
-def test_json_serial_datetime():
-    dt = datetime(2024, 1, 1)
-    assert main.json_serial(dt) == "2024-01-01T00:00:00"
-
-def test_json_serial_int():
-    with pytest.raises(TypeError):
-        main.json_serial(5)
 
 def test_get_tournaments_no_args(client, mocker):
     # Mock firebase auth
@@ -94,7 +56,6 @@ def test_get_tournaments_with_id(client, mocker):
     # Mock request
     m = mocker.MagicMock()
     m.args = {'id': 77}
-    m.headers = {'Authorization': 'Bearer test_token'}
     mocker.patch("main.request", m)
 
     # Mock get_db_connection
@@ -105,7 +66,10 @@ def test_get_tournaments_with_id(client, mocker):
     mock_execute.return_value.fetchall.return_value = mock_records
 
     # Make the request to the API
-    response = client.get('/api/v1/pga-tournaments')
+    response = client.get(
+        '/api/v1/pga-tournaments',
+        headers={'Authorization': 'Bearer test_token'}
+    )
 
     # Check that execute was called with the correct query
     expected_query = f"SELECT data FROM golfbetter.pga_tournaments where 1=1 and id = '{m.args['id']}'"
@@ -159,7 +123,6 @@ def test_get_players_with_id(client, mocker):
     # Mock request
     m = mocker.MagicMock()
     m.args = {'id': 77, 'tournamentId': 88}
-    m.headers = {'Authorization': 'Bearer test_token'}
     mocker.patch("main.request", m)
 
     # Mock get_db_connection
@@ -170,7 +133,10 @@ def test_get_players_with_id(client, mocker):
     mock_execute.return_value.fetchall.return_value = mock_records
 
     # Make the request to the API
-    response = client.get('/api/v1/pga-leaderboard-players')
+    response = client.get(
+        '/api/v1/pga-leaderboard-players',
+        headers={'Authorization': 'Bearer test_token'}
+    )
 
     # Check that execute was called with the correct query
     expected_query = f"SELECT data FROM golfbetter.pga_leaderboard_players where 1=1 and tournament_id = '{m.args['tournamentId']}' and id = '{m.args['id']}'"
@@ -224,7 +190,6 @@ def test_get_player_scorecards_with_id(client, mocker):
     # Mock request
     m = mocker.MagicMock()
     m.args = {'id': 77, 'tournamentId': 88}
-    m.headers = {'Authorization': 'Bearer test_token'}
     mocker.patch("main.request", m)
 
     # Mock get_db_connection
@@ -235,10 +200,14 @@ def test_get_player_scorecards_with_id(client, mocker):
     mock_execute.return_value.fetchall.return_value = mock_records
 
     # Make the request to the API
-    response = client.get('/api/v1/pga-player-scorecards')
+    response = client.get(
+        '/api/v1/pga-player-scorecards',
+        headers={'Authorization': 'Bearer test_token'}
+    )
 
     # Check that execute was called with the correct query
     expected_query = f"SELECT data FROM golfbetter.pga_player_scorecards where 1=1 and tournament_id = '{m.args['tournamentId']}' and id = '{m.args['id']}'"
+    print(response, m, m.args, expected_query, mock_execute.call_args)
     actual_query = mock_execute.call_args[0][0].text
     assert actual_query == expected_query, f"Expected query: {expected_query}, but got: {actual_query}"
 
